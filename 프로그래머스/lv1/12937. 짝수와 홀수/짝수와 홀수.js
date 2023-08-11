096c50cd947d3a0b292a84c5ab930aeda4849781
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-08-12 04:11:58 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,9 +1,4 @@
 function solution(num) {
-    let answer = '';
-    if (num % 2 === 0){
-        answer = "Even";
-    } else{
-        answer = "Odd";
-    }
+    const answer = (num % 2 === 0) ? "Even" : "Odd";
     return answer;
 }
