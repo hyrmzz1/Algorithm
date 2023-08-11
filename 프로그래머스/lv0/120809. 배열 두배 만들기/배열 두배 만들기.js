7ca979c216001b98bd915f9d413cc26a71bf93b3
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-08-11 21:36:22 +0900

    [level 0] Title: 배열 두배 만들기, Time: 0.06 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.js
@@ -1,7 +1,12 @@
 function solution(numbers) {
-    let answer = [];
+    //let answer = [];
+    /*
     for(let i = 0; i <= numbers.length - 1; i++){
         answer[i] = numbers[i] * 2;
     }
+    */
+    
+    const answer = numbers.map((x) => x * 2);
+    
     return answer;
 }
