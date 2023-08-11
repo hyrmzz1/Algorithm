27194e33258ad95a70374e00d54800dcb9f1b6a0
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-08-12 02:36:18 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.04 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/README.md
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/README.md
@@ -1,10 +1,10 @@
 # [level 0] 짝수는 싫어요 - 120813 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120813?language=javascript) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.05 ms
+메모리: 33.6 MB, 시간: 0.04 ms
 
 ### 구분
 
--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.js
@@ -1,12 +1,10 @@
 function solution(n) {
-    let answer = [];
-    for(i = 1; i <= n; i++){  // n 이하의 홀수 배열에 담기
-        if(i % 2 === 1){
-            answer.push(i);
-        }
+    const answer = [];  // 배열 안 요소가 추가되도 배열 자체가 재정의된건 아니니까 const 사용.
+    for(i = 1; i <= n; i += 2){ // i가 계속 홀수
+        answer.push(i);
     }
     
-    // 배열 요소 오름차순할 필요가 없다. n 이하의 홀수 작은 순으로 판별해서 push 하니까!
+    // 배열 요소 오름차순할 필요가 없다. 조건문에서 i++ 이므로 홀수 작은 순으로 push 하니까!
     /*
     answer = answer.sort(function(a, b){
         return a - b;
