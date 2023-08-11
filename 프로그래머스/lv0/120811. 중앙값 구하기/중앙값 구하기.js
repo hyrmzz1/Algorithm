6b9a0a96afe116616c676ff2a1daf9b9a4525a36
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-08-12 02:11:44 +0900

    [level 0] Title: 중앙값 구하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
+++ 프로그래머스/lv0/120811. 중앙값 구하기/중앙값 구하기.js
@@ -3,8 +3,18 @@ function solution(array) {
     array.sort(function(a, b){
         return a - b;
     });
+    
     // 중앙값 구하기
-    const mid = Math.floor((array.length - 1) / 2);
+    const mid = (array.length - 1) / 2;
+    /*
+    array의 길이는 홀수로 제한되므로 array.length - 1은 짝수.
+    따라서 mid는 항상 정수이므로 Math.floor() 사용할 필요 없음.
+    
+    만약 array.length - 1이 홀수여서 mid가 소수점 아래 값이 있다면?
+    배열의 인덱스값으로 정수가 아닌 값이 들어갈 수 있을까?
+    -> 배열의 인덱스는 0과 양의 정수여야 함.
+    따라서 index 값이 정수가 아니라면 undefined 반환.
+    */
     const answer = array[mid];
     
     return answer;
