8b59f2b5713ffbc9bfd7e9dd022f0a4511097322
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-08-14 17:38:23 +0900

    [level 0] Title: 피자 나눠 먹기 (3), Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
+++ 프로그래머스/lv0/120816. 피자 나눠 먹기 （3）/피자 나눠 먹기 （3）.js
@@ -1,14 +1,13 @@
 function solution(slice, n) {
     let answer = 0;
-    
+    /*
     if (slice >= n){
         answer = 1;
     } else{     // slice < n
-        if(n % slice === 0){
-            answer = n / slice;
-        } else{
-            answer = Math.floor(n / slice) + 1;
-        }
+        answer = Math.ceil(n / slice);
     }
+    */
+    answer = (slice >= n) ? 1 : Math.ceil(n / slice);
+    
     return answer;
 }
