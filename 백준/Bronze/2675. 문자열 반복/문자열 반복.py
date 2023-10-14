9078d7e30d0a4eee8f614c93739cdd0e581f3a9f
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-10-14 16:30:34 +0900

    [Bronze II] Title: 문자열 반복, Time: 108 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/2675. 문자열 반복/문자열 반복.py
@@ -0,0 +1,13 @@
+import sys
+# 테스트 케이스 개수 입력받기
+tc = int(sys.stdin.readline())
+
+# 테스트 케이스 개수만큼 반복문 실행
+for i in range(tc):
+    r, s = sys.stdin.readline().rstrip().split()
+    r = int(r)
+
+    p = ''
+    for i in s:
+        p += i * r
+    print(p)
