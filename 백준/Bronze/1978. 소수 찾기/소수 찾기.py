dfe26f9343a54c885cf9c049fd41765ffe950fd1
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-10-16 11:45:28 +0900

    [Bronze II] Title: 소수 찾기, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.py
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.py
@@ -1,17 +1,15 @@
 import sys
-n_num = sys.stdin.readline()    # 수의 개수
-n = list(map(int, sys.stdin.readline().split()))    # n개의 수 입력
+import math
+n = int(sys.stdin.readline())   # 테스트케이스 개수
+num = list(map(int, sys.stdin.readline().split()))
 
-# n개의 수 중 소수 개수 (리스트 n에서 소수 개수)
 prime_cnt = 0
-for i in n:
-    # 소수 판별. 소수는 1과 자기 자신으로만 나눠지는 수.
+for i in num:
     if i < 2:
         continue
-    else:   # i는 2 이상의 수
-        for j in range(2, i):   # i가 1이 아닐 때 실행
-            if i % j == 0:
-                break
-        else:    
-            prime_cnt += 1
+    for j in range(2, int(math.sqrt(i))+1):
+        if i % j == 0:
+            break
+    else:
+        prime_cnt += 1
 print(prime_cnt)
