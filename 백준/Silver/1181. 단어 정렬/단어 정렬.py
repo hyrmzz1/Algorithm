787d14463ac22ec939f9444c7cc11dd07573dbd8
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-10-16 16:25:20 +0900

    [Silver V] Title: 단어 정렬, Time: 204 ms, Memory: 115172 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.py
@@ -0,0 +1,11 @@
+import sys
+n = int(sys.stdin.readline())
+tc =[sys.stdin.readline().strip() for i in range(n)]
+
+# 중복 단어 제거
+tc = list(set(tc))  # sort()는 리스트 메서드이므로 다시 리스트형으로 변환.
+
+# 길이 짧은 순으로 정렬 후 길이 같으면 사전 순으로 정렬
+tc.sort(key = lambda x : (len(x), x))
+for i in tc:
+    print(i)
