c708d598cc522377bf26302d269580f81a5b2f69
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-10-31 13:22:47 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,14 +1,13 @@
 import sys
 n = int(sys.stdin.readline().rstrip())
 
-d = [0] * (n + 1)
-# top-down. 재귀 & 메모이제이션
-def fibo(x):
-    if x == 1 or x == 2:
-        return 1
-    if d[x] != 0:   # 이미 처리되었다면
-        return d[x] # 그대로 반환
-    d[x] = fibo(x - 1) + fibo(x - 2)    # 아직 계산하지 않은 x에 대해서만 피보나치
-    return d[x]
-
-print(fibo(n))
+# bottom-up. 반복문 사용
+if n <= 2:
+    print(1)
+else:
+    d = [0] * (n + 1)
+    d[1] = 1
+    d[2] = 1
+    for i in range(3, n + 1):
+        d[i] = d[i - 1] + d[i - 2]
+    print(d[n])
