c4d4593ded981dd798d2dbd189a7139c83c0cba9
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-11-10 19:41:50 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2588. 곱셈/곱셈.py
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -1,12 +1,7 @@
 import sys
 n = [int(sys.stdin.readline()) for _ in range(2)]
 
-a = n[0] * (n[1] % 10)
-b = n[0] * ((n[1] % 100) // 10)
-c = n[0] * (n[1] // 100)
-d = a + 10 * b + 100 * c
-
-print(a)
-print(b)
-print(c)
-print(d)
+print(n[0] * (n[1] % 10))
+print(n[0] * ((n[1] % 100) // 10))
+print(n[0] * (n[1] // 100))
+print(n[0] * n[1])
