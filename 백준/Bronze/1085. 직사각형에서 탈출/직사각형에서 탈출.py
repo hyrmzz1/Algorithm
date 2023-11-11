72281b1a2b3482599d3d0d5f70f2d27783aac0e6
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-11-11 14:16:17 +0900

    [Bronze III] Title: 직사각형에서 탈출, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
+++ 백준/Bronze/1085. 직사각형에서 탈출/직사각형에서 탈출.py
@@ -1,5 +1,3 @@
 import sys
-n = list(map(int, sys.stdin.readline().split()))    # x, y, w, h
-
-ans = min(n[0], n[2] - n[0], n[1], n[3] - n[1]) # x, w-x, y, h-y 중 최소값
-print(ans)
+x, y, w, h = map(int, sys.stdin.readline().split())
+print(min(x, w - x, y, h - y))
