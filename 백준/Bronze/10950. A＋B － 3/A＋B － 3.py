89b319a85abb2fc14d3ccaaa3410888af104975c
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-11-11 14:30:08 +0900

    [Bronze V] Title: A+B - 3, Time: 44 ms, Memory: 31120 KB

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,6 +1,6 @@
 import sys
 t = int(sys.stdin.readline())   # tc 개수
-n = [list(map(int, sys.stdin.readline().split())) for _ in range(t)]
 
-for i in range(t):
-    print(n[i][0] + n[i][1])
+for _ in range(t):
+    a, b = map(int, sys.stdin.readline().split())
+    print(a + b)
