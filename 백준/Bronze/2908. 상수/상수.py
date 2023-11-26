7552df52ce28699d121c6994755ac6d910381883
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-11-26 19:39:16 +0900

    [Bronze II] Title: 상수, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.py
+++ 백준/Bronze/2908. 상수/상수.py
@@ -1,7 +1,7 @@
 import sys
 a, b = sys.stdin.readline().split()
 
-reverse_a = int(a[2] + a[1] + a[0])
-reverse_b = int(b[2] + b[1] + b[0])
+reverse_a = int(a[::-1])
+reverse_b = int(b[::-1])
 
 print(reverse_a) if reverse_a > reverse_b else print(reverse_b)
