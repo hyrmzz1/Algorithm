d298599ed0518a29833aa5c6f70ed9b0f389df9a
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2023-12-14 19:08:32 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,11 +1,12 @@
-n = int(input())
+import sys
+c = int(sys.stdin.readline())    # tc 개수
 
-for _ in range(n):
-    nums = list(map(int, input().split()))
-    avg = sum(nums[1:])/nums[0]  # 평균을 구함 (nums[0]: 학생수, nums[1:] 점수)
+for _ in range(c):
+    n = list(map(int, sys.stdin.readline().split()))
+    avg = (sum(n) - n[0]) / n[0]
     cnt = 0
-    for score in nums[1:]:
-        if score > avg:
-            cnt += 1  # 평균 이상인 학생 수
-    rate = cnt/nums[0] *100
+    for i in range(1, len(n)):
+        if n[i] > avg:
+            cnt += 1
+    rate = cnt * 100 / n[0]
     print(f'{rate:.3f}%')
