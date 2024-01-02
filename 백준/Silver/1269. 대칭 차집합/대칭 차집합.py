ab43dc84dd4d5834ecb0ea0eab489bc71a08da49
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-01-02 21:16:18 +0900

    [Silver IV] Title: 대칭 차집합, Time: 260 ms, Memory: 87452 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -3,5 +3,5 @@ an, bn = map(int, sys.stdin.readline().split())
 a = set(map(int, sys.stdin.readline().split()))
 b = set(map(int, sys.stdin.readline().split()))
 
-inter = a & b    # 교집합
-print(an + bn - 2 * len(inter))
+xor = a ^ b    # 대칭차집합
+print(len(xor))
