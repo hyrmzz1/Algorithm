d8b3e67bbcedbb9ab10845f011d421766a5890b4
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-01-03 19:21:50 +0900

    [Gold IV] Title: 문자열 폭발, Time: 752 ms, Memory: 42300 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,15 @@
+import sys
+string = sys.stdin.readline().rstrip()
+bomb = sys.stdin.readline().rstrip()
+stack = []
+
+for i in string:
+    stack.append(i)
+    if ''.join(stack[-(len(bomb)):]) == bomb:
+        for _ in range(len(bomb)):
+            stack.pop()
+
+if stack:
+    print(''.join(stack))
+else:
+    print("FRULA")
