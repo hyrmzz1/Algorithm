5e2c98dcc871a6c30f5cec5dd799b56a47028047
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-01-07 17:41:46 +0900

    [Bronze II] Title: 블랙잭, Time: 104 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,12 +1,14 @@
 import sys
-import itertools
 n, m = map(int, sys.stdin.readline().split())
 cards = list(map(int, sys.stdin.readline().split()))
-cPn = itertools.permutations(cards, 3)
 
+cards_sum = 0
 max_sum = 0
-for i in cPn:
-    if sum(i) <= m:
-        max_sum = max(max_sum, sum(i))
+for i in range(0, n):
+    for j in range(i + 1, n):
+        for k in range(j + 1, n):
+            cards_sum = cards[i] + cards[j] + cards[k]
+            if cards_sum <= m:
+                max_sum = max(max_sum, cards_sum)
 
 print(max_sum)
