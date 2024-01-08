4b496c0757a4de9f85663596f2d0b01b1fb9dfe0
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-01-08 15:13:29 +0900

    [Silver V] Title: 숫자 카드, Time: 700 ms, Memory: 125176 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,13 +1,11 @@
 import sys
 n = int(sys.stdin.readline())
-cards = list(map(int, sys.stdin.readline().split()))
+cards = set(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 num = list(map(int, sys.stdin.readline().split()))
 
-card_dict = {i: 0 for i in cards}    # 딕셔너리 컴프리헨션
-
 for i in num:
-    if i in card_dict:
+    if i in cards:
         print(1, end=" ")
     else:
         print(0, end=" ")
