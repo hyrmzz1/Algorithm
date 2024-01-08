4572fd8afc63adf3b0eeead8890edd9e197c448f
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-01-08 15:36:17 +0900

    [Silver V] Title: 숫자 카드, Time: 1904 ms, Memory: 112884 KB -BaekjoonHub
    이분탐색

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,11 +1,26 @@
 import sys
 n = int(sys.stdin.readline())
-cards = set(map(int, sys.stdin.readline().split()))
+cards = list(map(int, sys.stdin.readline().split()))
 m = int(sys.stdin.readline())
 num = list(map(int, sys.stdin.readline().split()))
 
+def binary_search(arr, target):
+    start = 0
+    end = len(arr) - 1
+
+    while(start <= end):
+        mid = (start + end) // 2
+
+        if arr[mid] == target:
+            return True
+        elif arr[mid] > target:
+            end = mid - 1
+        else:
+            start = mid + 1
+
+cards.sort()
 for i in num:
-    if i in cards:
-        print(1, end=" ")
+    if binary_search(cards, i): # True
+        print(1, end = " ")
     else:
-        print(0, end=" ")
+        print(0, end = " ")
