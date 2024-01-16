f1b732e6bbd5675669429ba27b338ac627ca0d4c
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-01-17 01:29:32 +0900

    [Silver II] Title: 랜선 자르기, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -1,19 +1,17 @@
-N, K = map(int, input().split())
-list = []
-for _ in range(N):
-    list.append(int(input()))
+import sys
+k, n = map(int, sys.stdin.readline().split())
+k_list = [int(sys.stdin.readline()) for _ in range(k)]
 
-s = 1   # 최소 길이
-e = max(list)   # 최대 길이
+start = 1
+end = max(k_list)
 
-while s <= e:
-    mid = (s + e) // 2
-    LAN = 0
-    for i in list:
-        LAN += i // mid   # 잘린 랜선 개수
-    if LAN >= K:   # K개 이상 만들 수 있는 경우
-        s = mid + 1   # mid+1부터 e까지 탐색
-    else:   # K만큼 만들 수 없는 경우
-        e = mid - 1   # s부터 mid-1까지 탐색
-
-print(e)
+while start <= end:
+    mid = (start + end) // 2
+    lan = 0 # 랜선 개수
+    for i in k_list:
+        lan += i // mid
+    if lan < n:
+        end = mid - 1
+    else:
+        start = mid + 1
+print(end)
