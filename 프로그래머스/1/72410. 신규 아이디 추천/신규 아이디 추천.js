278ba47ad767c911f4307634e3ff4faedbb300ce
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-03-27 18:06:55 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.43 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,29 +1,10 @@
 function solution(new_id) {
-    new_id = new_id.toLowerCase(); // 1단계
+    new_id = new_id.toLowerCase() // 1단계
+        .replace(/[^\w|\-|\.]/g, '') // 2단계
+        .replace(/[.]{2,}/g, '.')    // 3단계
+        .replace(/^\.|\.$/g, '')    // 4단계
+        .padEnd(1, "a")  // 5단계
+        .slice(0, 15).replace(/\.$/, '');   // 6단계
     
-    new_id = new_id.replace(/[^\w|\-|\.]/g, ''); // 2단계
-    
-    new_id = new_id.replace(/[.]{2,}/g, '.');    // 3단계
-    
-    new_id = new_id.replace(/^\.|\.$/g, '');    // 4단계
-    
-    if (new_id.length === 0) new_id = 'a';  // 5단계
-    
-    // 6단계
-    if (new_id.length >= 16) {
-        new_id = new_id.slice(0, 15);
-        
-        if (new_id.charAt(new_id.length - 1) === '.') {
-            new_id = new_id.slice(0, new_id.length - 1);
-        }
-    }
-    
-    // 7단계
-    if (new_id.length <= 2) {
-        while (new_id.length < 3) {
-            new_id += new_id.charAt(new_id.length - 1)
-        }
-    }
-    
-    return new_id;
+    return new_id.padEnd(3, new_id.charAt(new_id.length - 1));  // 7단계
 }
