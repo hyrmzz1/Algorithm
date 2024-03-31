c122ab7eba51ca61e99da4d21d2c88c787f7fa00
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-01 03:19:48 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.js
+++ 프로그래머스/2/42747. H－Index/H－Index.js
@@ -1,8 +1,7 @@
 function solution(citations) {
     citations = citations.sort((a, b) => b - a);
-    let h = 0;
-    let h_max = 0;
     
+    let h = 0;
     while(h < citations[0]) {
         h++;
         citations.shift();
