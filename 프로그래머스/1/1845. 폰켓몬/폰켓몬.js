4e0ad9094fbacfc1e91f945595efa6c2db6b3652
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-01 20:07:30 +0900

    [level 1] Title: 폰켓몬, Time: 0.25 ms, Memory: 33.9 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,10 +1,6 @@
 function solution(nums) {
     let answer = nums.length / 2; // 선택 가능한 종류의 최대 개수
-    let hashMap = new Map();    // 해시 테이블
-    
-    nums.forEach((num, idx) => {
-        hashMap.set(num, idx);
-    })
+    const hashMap = new Set([...nums])  // 해시 테이블
     
     return answer <= hashMap.size ? answer : hashMap.size;
 }
