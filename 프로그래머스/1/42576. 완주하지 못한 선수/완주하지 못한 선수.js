449f96eaeffc7d19d7040e801f729f6455f522c9
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-01 21:28:12 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 32.47 ms, Memory: 66.6 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,22 +1,12 @@
-function solution(participant, completion) {    // (참가자 이름, 완주선수 이름)
-    let answer = '';
+function solution(participant, completion) {
     const hashMap = new Map();
     
     participant.forEach((name) => {
-        if (!hashMap.has(name)) {   // 동명이인 없으면
-            hashMap.set(name, 1);
-        } else {    // 동명이인 있으면
-            hashMap.set(name, hashMap.get(name) + 1);
-        }
+        !hashMap.has(name) ? hashMap.set(name, 1) : hashMap.set(name, hashMap.get(name) + 1);
     })
     
     completion.forEach((name) => {
-        if (hashMap.get(name) === 1) {  // 동명이인 없으면
-            hashMap.delete(name);   // 삭제
-        }
-        else{   // 동명이인 있으면
-            hashMap.set(name, hashMap.get(name) - 1);   // 동명이인 수 - 1
-        }
+        hashMap.get(name) === 1 ? hashMap.delete(name) : hashMap.set(name, hashMap.get(name) - 1);
     })
     
     return [...hashMap.entries()][0][0];
