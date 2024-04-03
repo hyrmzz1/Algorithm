456f9546b69be348f0a7e46a80157d95bb64ee81
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-03 19:18:08 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.20 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.js
@@ -0,0 +1,34 @@
+function solution(id_list, report, k) { // 전체 유저, 누가 누구 신고했는지, 정지 기준 횟수
+    const user = id_list.map((id) => {
+        return {
+            id: id,
+            report_id: [],  // 이 사용자가 신고한 사용자 목록
+            reported_cnt: 0,    // 이 사용자가 신고당한 횟수
+        }
+    });
+    
+    // 신고한 유저 기록, 신고당한 횟수 기록
+    report.forEach(i => {
+        let a = i.split(' ')[0];
+        let b = i.split(' ')[1];
+        
+        // 동일 유저 여러번 신고해도 신고 횟수 늘어나지 않음
+        if (!user.find(u => u.id === a).report_id.includes(b)) {
+            user.find(u => u.id === a).report_id.push(b);
+            user.find(u => u.id === b).reported_cnt += 1;
+        }
+    })
+    
+    // 정지 유저
+    let stop_id = user.filter(u => u.reported_cnt >= k)
+                        .map(u => u.id)
+    
+    user.forEach(u => {
+        u.mail = 0;
+        u.report_id.forEach(id => {
+            if (stop_id.includes(id)) u.mail += 1;
+        })
+    })
+    
+    return user.map(u => u.mail);
+}
