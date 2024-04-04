e9b8b64d84217d7c3a7fc2e7d6348855c63db342
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-05 00:00:19 +0900

    [level 1] Title: 모의고사, Time: 26.75 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -0,0 +1,48 @@
+function solution(answers) {
+    answers = answers.join('');
+    
+    const one = {
+        name: 1,
+        type: "12345",
+        ans: 0
+    };
+    
+    const two = {
+        name: 2,
+        type: "21232425",
+        ans: 0
+    };
+    
+    const three = {
+        name: 3,
+        type: "3311224455",
+        ans: 0
+    };
+    
+    while (one.type.length <= answers.length || two.type.length <= answers.length || three.type.length <= answers.length) {
+        one.type += one.type;
+        two.type += two.type;
+        three.type += three.type;
+    }
+    
+    for (let i = 0; i < answers.length; i++) {
+        if (one.type[i] === answers[i]) one.ans += 1;
+        if (two.type[i] === answers[i]) two.ans += 1;
+        if (three.type[i] === answers[i]) three.ans += 1;
+    }
+    
+    let maxAns = 0, maxPerson = [];
+    [one, two, three].forEach(obj => {
+        if (obj.ans >= maxAns) {
+            maxAns = obj.ans;
+        }
+    });
+    
+    [one, two, three].forEach(obj => {
+        if (obj.ans === maxAns) {
+            maxPerson.push(obj.name);
+        }
+    });
+    
+    return maxPerson;
+}
