b435f2658c42a364ca272f9e664e85532feda094
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-05 02:36:40 +0900

    [level 1] Title: 모의고사, Time: 4.61 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -1,47 +1,32 @@
 function solution(answers) {
-    answers = answers.join('');
-    
     const one = {
         name: 1,
-        type: "12345",
+        type: [1, 2, 3, 4, 5],
         ans: 0
     };
     
     const two = {
         name: 2,
-        type: "21232425",
+        type: [2, 1, 2, 3, 2, 4, 2, 5],
         ans: 0
     };
     
     const three = {
         name: 3,
-        type: "3311224455",
+        type: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5],
         ans: 0
     };
     
-    while (one.type.length <= answers.length || two.type.length <= answers.length || three.type.length <= answers.length) {
-        one.type += one.type;
-        two.type += two.type;
-        three.type += three.type;
-    }
-    
     for (let i = 0; i < answers.length; i++) {
         [one, two, three].forEach(obj => {
-            if (obj.type[i] === answers[i]) obj.ans += 1;
+            if (obj.type[i % obj.type.length] === answers[i]) obj.ans += 1;
         });
     }
     
-    let maxAns = 0, maxPerson = [];
+    let maxAns = Math.max(one.ans, two.ans, three.ans);
+    let maxPerson = [];
     [one, two, three].forEach(obj => {
-        if (obj.ans >= maxAns) {
-            maxAns = obj.ans;
-        }
-    });
-    
-    [one, two, three].forEach(obj => {
-        if (obj.ans === maxAns) {
-            maxPerson.push(obj.name);
-        }
+        if (obj.ans === maxAns) maxPerson.push(obj.name);
     });
     
     return maxPerson;
