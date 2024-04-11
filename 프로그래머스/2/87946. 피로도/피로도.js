d07f4ce2216ced112742d4d6c6d01dd3f08cea51
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-11 20:07:00 +0900

    [level 2] Title: 피로도, Time: 0.59 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -0,0 +1,45 @@
+function permutate(arr) {
+    const cases = [];   // 탐험 순서 경우의 수
+    
+    // DFS
+    const dfs = (i, arr) => {
+        // base condition
+        if (i === arr.length) {
+            return cases.push([...arr]);
+        }
+        
+        for (let j = i; j < arr.length; j++) {
+            // swap
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+            
+            //dfs
+            dfs(i + 1, arr);
+            
+            // swap back (백트랙킹)
+            [arr[i], arr[j]] = [arr[j], arr[i]];
+        }
+    }
+    dfs(0, arr);
+    return cases;
+}
+
+function solution(k, dungeons) {
+    const casePath = permutate(dungeons.map((dungeon, idx) => idx));
+    let ans = 0;
+    
+    casePath.forEach(path => {
+        let piro = k;
+        let cnt = 0;
+        
+        path.forEach(i => {
+            if (piro >= dungeons[i][0]) {
+                piro -= dungeons[i][1];
+                cnt ++;
+            } else return false;
+        })
+        
+        ans = Math.max(ans, cnt)
+    })
+    
+    return ans;
+}
