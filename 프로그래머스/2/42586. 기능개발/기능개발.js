a877e6770be1c68dc498c08f3e21d62b3887400b
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-20 17:49:50 +0900

    [level 2] Title: 기능개발, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.js
+++ 프로그래머스/2/42586. 기능개발/기능개발.js
@@ -1,12 +1,12 @@
 function solution(progresses, speeds) {
     const days = [];
-    for (let i = 0; i < progresses.length; i++) {
-        days.push(Math.ceil((100 - progresses[i]) / speeds[i]));
-    }
+    progresses.map((progress, idx) => {
+        days.push(Math.ceil((100 - progress) / speeds[idx]));
+    })
     
     const ans = [];
     let cnt = 1;
-    let maxDay = days[0]
+    let maxDay = days[0];
     for (let i = 1; i < days.length; i++) {
         if (maxDay >= days[i]) {
             cnt++;
