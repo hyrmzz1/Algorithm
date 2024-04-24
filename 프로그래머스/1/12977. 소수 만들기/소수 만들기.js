ac1b57629f2b32d93f153686459cfcd2c30da10e
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-25 00:09:21 +0900

    [level 1] Title: 소수 만들기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.js
@@ -1,21 +1,23 @@
+function isPrime(num) {
+    if (num <= 1) return false;
+    
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+        if (num % i === 0) return false;
+    }
+    
+    return true;
+}
+
 function solution(nums) {
-    const sum = []
+    let cnt = 0
     for (let i = 0; i < nums.length - 2; i++) {
         for (let j = i + 1; j < nums.length - 1; j++) {
             for (let k = j + 1; k < nums.length; k++) {
-                sum.push(nums[i] + nums[j] + nums[k]);
+                let num = nums[i] + nums[j] + nums[k];
+                if(isPrime(nums[i] + nums[j] + nums[k])) cnt++;
             }
         }
     }
-    
-    // 소수 판별
-    let cnt = 0;
-    sum.forEach(s => {
-        for (let i = 2; i <= Math.sqrt(s); i++) {
-            if (s % i === 0) return false;
-        }
-        cnt++;
-    })
     
     return cnt;
 }
