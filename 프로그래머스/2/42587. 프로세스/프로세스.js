74cc9b7af5a8786c79a5bd3a523d61c231a484cb
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-04-25 22:22:31 +0900

    [level 2] Title: 프로세스, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.js
+++ 프로그래머스/2/42587. 프로세스/프로세스.js
@@ -1,9 +1,9 @@
 function solution(priorities, location) {
     let dict = priorities.map((priority, idx) => [priority, idx])   // 실행 대기
+    let maxProcess = Math.max(...dict.map(x => x[0]));
     const runIdx = [];
     
     while (dict.length) {
-        let maxProcess = Math.max(...dict.map(x => x[0]));
         let [currProcess, currIdx] = dict.shift();
         
         if (currProcess < maxProcess) {
@@ -11,6 +11,7 @@ function solution(priorities, location) {
         } else {
             runIdx.push(currIdx);
             if (currIdx === location) return runIdx.length;
+            maxProcess = Math.max(...dict.map(x => x[0]));  // 최대값 갱신
         }
     }
 }
