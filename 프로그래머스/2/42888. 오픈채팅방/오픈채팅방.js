841e0680381ec6c4eede3a969483b3a086fd1463
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-05-31 17:35:06 +0900

    [level 2] Title: 오픈채팅방, Time: 180.23 ms, Memory: 94.3 MB -BaekjoonHub

--- 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
+++ 프로그래머스/2/42888. 오픈채팅방/오픈채팅방.js
@@ -1,24 +1,23 @@
 function solution(record) {
-    const obj = new Object();
+    const userMap = new Map();
     const message = [];
     const temp = [];
     
     record.forEach(elem => {
         const [action, uid, nickname] = elem.split(' ');
         if (action === "Enter") {
-            obj[uid] = nickname;
+            userMap.set(uid, nickname);
             temp.push(uid + "님이 들어왔습니다.");
         } else if (action === "Leave") {
             temp.push(uid + "님이 나갔습니다.");
         } else {    // action === "Change"
-            obj[uid] = nickname;
+            userMap.set(uid, nickname);
         }
     })
     
     temp.forEach(elem => {
         const uid = elem.slice(0, elem.indexOf('님이'));
-        const str = elem.replace(uid, obj[uid]);  // uid -> nickname
-        message.push(str);
+        message.push(elem.replace(uid, userMap.get(uid)));  // uid -> nickname
     })
     
     return message;
