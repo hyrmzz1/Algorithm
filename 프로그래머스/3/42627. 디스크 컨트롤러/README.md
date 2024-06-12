b4bd8bfdcba3c1ce17e9b81812847b468e5db8b7
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-06-12 20:09:31 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.08 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/README.md
+++ 프로그래머스/3/42627. 디스크 컨트롤러/README.md
@@ -1,10 +1,10 @@
 # [level 3] 디스크 컨트롤러 - 42627 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42627#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.11 ms
+메모리: 33.4 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 12일 20:07:49
+2024년 06월 12일 20:09:29
 
 ### 문제 설명
 
--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.js
@@ -6,7 +6,7 @@ function solution(jobs) {
     const tmp = [];
     let time = 0;
 
-    while (jobs.length > 0 || tmp.length > 0) { // 모든 작업 수행할 때까지 반복
+    while (jobs.length || tmp.length) { // 모든 작업 수행할 때까지 반복
         while (jobs.length > 0 && jobs[0][0] <= time) tmp.push(jobs.shift());
         
         if (tmp.length) {   // 해당 시점에 처리 가능한 작업 있다면
@@ -15,7 +15,7 @@ function solution(jobs) {
             answer += (time - req + fin);
             time += fin;
         } else {
-            time++;
+            time = jobs[0][0];  // 제일 빨리 요청들어오는 작업 시간으로
         }
     }
     
