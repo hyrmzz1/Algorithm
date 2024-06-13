9db9dde15e8b7c47529666457e86c66b52d13903
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-06-13 19:29:58 +0900

    [level 3] Title: 입국심사, Time: 0.18 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.js
+++ 프로그래머스/3/43238. 입국심사/입국심사.js
@@ -1,15 +1,10 @@
-function solution(n, times) {
-    times.sort((a, b) => a - b);
-    
-    let left = 0;
+function solution(n, times) {let left = 0;
     let right = n * times[times.length - 1];    // 가장 오래 걸리는 심사관에게 모든 사람이 심사받는 경우 (최대값)
     
     while (left <= right) {
         let mid = Math.floor((left + right) / 2);
         
-        const nSum = times.reduce((acc, curr) => {
-            return acc + Math.floor(mid / curr)
-        }, 0);
+        const nSum = times.reduce((acc, curr) => acc + Math.floor(mid / curr), 0);
         
         if (nSum >= n) { // mid분간 n명 이상 심사 가능
             right = mid - 1; // 시간 줄이기
