1d9477282d233d28b7275524b8add20e8889ba27
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-06-17 23:43:53 +0900

    [level 1] Title: 2016년, Time: 0.11 ms, Memory: 34 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.js
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -1,7 +1,7 @@
 function solution(a, b) {
     const week = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
     
-    const date = new Date(2016, a - 1, b).getDay();
+    const date = new Date(`2016-${a}-${b}`).getDay();
     
     return week[date];
 }
