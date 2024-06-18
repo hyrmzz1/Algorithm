bc55602b1a21e6ea6df7201226cdf5dd706c74a2
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-06-18 15:09:48 +0900

    [level 2] Title: 할인 행사, Time: 0.32 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,19 +1,16 @@
 function solution(want, number, discount) {
-    var answer = 0;
+    let answer = 0;
     
     for (let i = 0; i < discount.length; i++) {
         const list = discount.slice(i, i + 10);
-        let listCnt = [];
+        let flag = true;
         
-        want.forEach(w => {
-            listCnt.push(list.filter(elem => elem === w).length);
+        want.forEach((w, idx) => { 
+            // 원하는 만큼 할인구매 X
+            if (list.filter(elem => elem === w).length < number[idx]) flag = false;
         })
         
-        // listCnt와 number 비교
-        for (let j = 0; j < number.length; j++) {
-            if (listCnt[j] < number[j]) break;
-            if (j === number.length - 1) answer++;
-        }
+        if (flag) answer++; // 원하는 만큼 할인구매 가능한 날
     }
     
     return answer;
