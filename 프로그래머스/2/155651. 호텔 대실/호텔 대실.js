78e79e1b5c2716c5f80bf9ad01263fc7310e3e61
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-06-25 20:30:53 +0900

    [level 2] Title: 호텔 대실, Time: 9.60 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,6 +1,6 @@
 const toMinutes = (str) => {
-    const [hour, minute] = str.split(':');
-    return Number(hour) * 60 + Number(minute);
+    const [hour, minute] = str.split(':').map(x => Number(x));
+    return hour * 60 + minute;
 }
 
 function solution(bookTime) {
@@ -10,16 +10,8 @@ function solution(bookTime) {
         const startTime = toMinutes(start);
         const endTime = toMinutes(end) + 10;    // 청소 시간 포함
         
-        if (room.length === 0) {
-            room.push(endTime);
-        } else {
-            const idx = room.findIndex((elem) => elem <= startTime);
-            if (idx !== -1) {    // 사용 가능 객실 있으면
-                room[idx] = endTime;
-            } else {    // 없으면
-                room.push(endTime); // 방 추가
-            }
-        }
+        const idx = room.findIndex((elem) => elem <= startTime);    // -1이라면 사용 가능한 방 X
+        idx === -1 ? room.push(endTime) : room[idx] = endTime;  // 사용 가능한 방 있다면 해당 방의 사용 시간 갱신
     });
     
     return room.length;
