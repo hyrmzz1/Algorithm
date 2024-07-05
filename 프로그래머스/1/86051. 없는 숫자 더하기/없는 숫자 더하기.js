499dc0cfbf90dfd822aa47ecf258f427f8a414d5
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-07-05 20:01:28 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -1,5 +1,10 @@
 function solution(numbers) {
     // 코딩 테스트 합격자 되기 - 자바스크립트 편
-    const number = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-    return number.filter(n => !numbers.includes(n)).reduce((acc, cur) => acc + cur);
+    let answer = 0;
+    
+    for (let i = 0; i < 10; i++) {
+        if (!numbers.includes(i)) answer += i;
+    }
+    
+    return answer;
 }
