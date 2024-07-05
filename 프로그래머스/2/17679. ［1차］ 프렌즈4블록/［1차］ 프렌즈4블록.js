c65c69b35d5d87a08b1094da0ac3ecc9b968b7c5
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-07-05 17:10:21 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.90 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,13 +1,17 @@
 function solution(m, n, board) {
     board = board.map(col => col.split(''));
-    const newBoard = [...board];
     
     while (true) {
+        // 각 라운드
+        // const newBoard = [...board];    // 얕은 복사
+        const newBoard = board.map(row => [...row]);    // 2차원 배열 깊은 복사
         let removeBool = false;    // 사라질 블록 유무
         
         for (let i = 0; i < m - 1; i++) {
             for (let j = 0; j < n - 1; j++) {
-                if (board[i][j] === board[i][j+1] &&
+                if (
+                    board[i][j] !== '' &&
+                    board[i][j] === board[i][j+1] &&
                     board[i][j] === board[i+1][j] &&
                     board[i][j] === board[i+1][j+1]
                 ){
@@ -23,19 +27,19 @@ function solution(m, n, board) {
         if (!removeBool) break;  // 사라질 블록 없을 경우 게임 끝
         
         // 재정렬
-        for (let i = 0; i < m - 1; i++) {
-            for (let j = 0; j < n - 1; j++) {
-                return newBoard
-                if (newBoard[i+1][j] === '') {
-                    newBoard[i+1][j] = newBoard[i][j];
-                    newBoard[i][j] = '';
-                    
+        const reorderBoard = Array.from(Array(m), () => Array(n).fill(''));
+        for (let col = 0; col < n; col++) {
+            let newRow = m - 1;
+            for (let row = m - 1; row >= 0; row--) {
+                if (newBoard[row][col] !== '') {
+                    reorderBoard[newRow][col] = newBoard[row][col];
+                    newRow--;
                 }
             }
         }
         
-        board = [...newBoard];
+        board = reorderBoard.map(v => [...v]);
     }
     
-    return board;
+    return board.map(row => row.filter(elem => elem === '').length).reduce((a, b) => a + b);
 }
