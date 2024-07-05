7e3317d6028d36abadc27b56c66a13945f5c3f27
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-07-05 17:08:13 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 0.83 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,17 +1,13 @@
 function solution(m, n, board) {
     board = board.map(col => col.split(''));
+    const newBoard = [...board];
     
     while (true) {
-        // 각 라운드
-        // const newBoard = [...board];    // 얕은 복사
-        const newBoard = board.map(row => [...row]);    // 깊은 복사
         let removeBool = false;    // 사라질 블록 유무
         
         for (let i = 0; i < m - 1; i++) {
             for (let j = 0; j < n - 1; j++) {
-                if (
-                    board[i][j] !== '' &&
-                    board[i][j] === board[i][j+1] &&
+                if (board[i][j] === board[i][j+1] &&
                     board[i][j] === board[i+1][j] &&
                     board[i][j] === board[i+1][j+1]
                 ){
@@ -27,19 +23,19 @@ function solution(m, n, board) {
         if (!removeBool) break;  // 사라질 블록 없을 경우 게임 끝
         
         // 재정렬
-        const reorderBoard = Array.from(Array(m), () => Array(n).fill(''));
-        for (let col = 0; col < n; col++) {
-            let newRow = m - 1;
-            for (let row = m - 1; row >= 0; row--) {
-                if (newBoard[row][col] !== '') {
-                    reorderBoard[newRow][col] = newBoard[row][col];
-                    newRow--;
+        for (let i = 0; i < m - 1; i++) {
+            for (let j = 0; j < n - 1; j++) {
+                return newBoard
+                if (newBoard[i+1][j] === '') {
+                    newBoard[i+1][j] = newBoard[i][j];
+                    newBoard[i][j] = '';
+                    
                 }
             }
         }
         
-        board = reorderBoard.map(v => [...v]);
+        board = [...newBoard];
     }
     
-    return board.map(row => row.filter(elem => elem === '').length).reduce((a, b) => a + b);
+    return board;
 }
