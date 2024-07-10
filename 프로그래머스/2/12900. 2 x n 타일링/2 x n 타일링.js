0af740b29d3a2a3360dc86ec3ff8a36492e66284
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-07-10 22:24:18 +0900

    [level 2] Title: 2 x n 타일링, Time: 2.31 ms, Memory: 36.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.js
@@ -0,0 +1,12 @@
+// 피보나치
+function solution(n) {
+    const dp = Array(n + 1).fill(0);
+    dp[0] = 1;
+    dp[1] = 2;
+    
+    for (let i = 2; i < n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % 1000000007;
+    }
+    
+    return dp[n-1];
+}
