adf26fe97d3c37897c444f938d65208bd09c4895
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-07-10 21:49:32 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 104 ms, Memory: 10652 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
-
 const readFileSyncAddress = 'dev/stdin';
 // const readFileSyncAddress = 'test.txt';
+
 const input = fs.readFileSync(readFileSyncAddress).toString().trim().split('\n');
 const [n, k] = input[0].split(' ');
 
@@ -11,14 +11,10 @@ function solution(n, k) {
 
 	let cnt = 0;
 	while (queue.length > 0) {
-		if (cnt < k - 1) {
-			queue.push(queue.shift());
-			cnt++;
-		}
-		if (cnt === k - 1) {
-			answer.push(queue.shift());
-			cnt = 0;
-		}
+		cnt += k - 1;
+		cnt %= queue.length;
+		
+		answer.push(queue.splice(cnt, 1)[0]);
 	}
 
 	console.log(`<${answer.join(', ')}>`);
