fc981f305ac9b724bd1c651ee26ee354b8db5b3b
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-07-11 21:54:19 +0900

    [level 2] Title: 메뉴 리뉴얼, Time: 4.19 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/README.md
@@ -1,10 +1,10 @@
 # [level 2] 메뉴 리뉴얼 - 72411 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/72411) 
 
 ### 성능 요약
 
-메모리: 38 MB, 시간: 4.99 ms
+메모리: 38 MB, 시간: 4.19 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 11일 21:46:19
+2024년 07월 11일 21:54:17
 
 ### 문제 설명
 
--- 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
+++ 프로그래머스/2/72411. 메뉴 리뉴얼/메뉴 리뉴얼.js
@@ -1,11 +1,12 @@
 const getCombination = (str, len, map) => {
     const combine = (prefix, start) => {
         if (prefix.length === len) {
-            prefix = [...prefix].sort().join('');   // 오름차순 정렬
+            prefix = [...prefix].sort().join('');   // XW와 WX는 같다
             map.has(prefix) ? map.set(prefix, map.get(prefix) + 1) : map.set(prefix, 1);
+            
             return;
         }
-    
+        
         for (let i = start; i < str.length; i++) {
             combine(prefix + str[i], i + 1);
         }
@@ -20,17 +21,14 @@ function solution(orders, course) {
     course.forEach(cnt => {
         // 1. cnt별 조합 구하기
         const menu = new Map();   // key: 조합, value: 주문 횟수
-        
         orders.forEach(order => {
             getCombination(order, cnt, menu);
-            
         })
         
         // 2. cnt별 가장 많이 주문된 조합 구하기
         const maxCnt = Math.max(...new Set([...menu.values()]));
         menu.forEach((value, key) => {
-            if (value == maxCnt && maxCnt !== 1) {
-                answer.push(key);}
+            if (value == maxCnt && maxCnt !== 1) answer.push(key);
         })
     })
     
