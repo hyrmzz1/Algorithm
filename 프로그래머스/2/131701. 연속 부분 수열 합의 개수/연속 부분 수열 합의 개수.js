52c5f52663a426d3f946daaae36db212972eb234
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-08-14 19:06:25 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1158.51 ms, Memory: 53 MB -BaekjoonHub

---
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.js
@@ -0,0 +1,14 @@
+function solution(elements) {
+    const answer = new Set();
+    const len = elements.length;
+    elements.push(...elements);
+    
+    // 연속 부분 수열 길이
+    for (let i = 1; i <= len; i++) {
+        for (let j = 0; j < len; j++){
+            answer.add(elements.slice(j, j + i).reduce((a, b) => a + b))
+        }
+    }
+    
+    return answer.size;
+}
