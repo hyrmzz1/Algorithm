6f9f1eaf46941b625d9ac78c1ab9ab35bbea46c8
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-08-18 17:04:10 +0900

    [level 1] Title: 과일 장수, Time: 247.17 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.js
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.js
@@ -1,16 +1,13 @@
 function solution(k, m, score) {
-    const box = parseInt(score.length / m); // 판매할 박스 수
-    const remainder = score.length % m; // 고려 X
-
     // 포장하지 않는 사과 -> 점수 낮은 품목
-    score.sort((a, b) => a - b);
-    score = score.slice(remainder);
+    score = score.sort((a, b) => a - b).slice(score.length % m);
     
     // 최대 이익 얻도록 박스 구성
-    let minAppleScore = 0;  // 최저 사과 점수의 합
+    // 박스 개수만큼 반복
+    let answer = 0;
     for (let i = 0; i < score.length; i += m) {
-        minAppleScore += score[i];
+        answer += score[i] * m;
     }
     
-    return minAppleScore * m;
+    return answer;
 }
