b09a2ccb31e7a9f59bd15c210f1841359ea37354
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-09-04 18:56:17 +0900

    [level 1] Title: 문자열 나누기, Time: 1.41 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
+++ 프로그래머스/1/140108. 문자열 나누기/문자열 나누기.js
@@ -2,16 +2,14 @@ function solution(s) {
     s = [...s];
     let answer = 0;
     
-    let x = s.shift();
-    let xCnt = 1;
+    let x = s[0];
+    let xCnt = 0;
     let notXCnt = 0;
     let bool = false;   // x 변경 여부 기록
     
-    while (s.length) {
-        let str = s.shift();
-        if (bool) x = str;
-        
-        str === x ? xCnt++ : notXCnt++;
+    s.forEach(elem => {
+        if (bool) x = elem;
+        elem === x ? xCnt++ : notXCnt++;
         
         if (xCnt === notXCnt) {
             answer++;
@@ -19,7 +17,7 @@ function solution(s) {
         } else {
             bool = false;
         }
-    }
+    })
     
     return xCnt === notXCnt ? answer : ++answer;
 }
