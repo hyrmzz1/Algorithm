5114b73ec359f3f0ad69ee6032b47ecc0f5faed5
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-09-07 19:22:50 +0900

    [level 2] Title: 가장 큰 정사각형 찾기, Time: 46.33 ms, Memory: 82.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12905. 가장 큰 정사각형 찾기/가장 큰 정사각형 찾기.js
@@ -0,0 +1,16 @@
+function solution(board) {
+    if (board.length <= 1 || board[0].length <= 1) return 1;
+    
+    let answer = 0;
+    
+    for (let i = 1; i < board.length; i++) {
+        for (let j = 1; j < board[0].length; j++) {
+            if (board[i][j] !== 0) {
+                board[i][j] = Math.min(board[i-1][j-1], board[i-1][j], board[i][j-1]) + 1;
+                answer = Math.max(answer, board[i][j]);
+            }
+        }
+    }
+    
+    return answer * answer;
+}
