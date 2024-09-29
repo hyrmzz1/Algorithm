b2357a082cb41ae2af9b388d7c9671b4e3dfcf76
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-09-30 00:02:20 +0900

    [level 1] Title: 둘만의 암호, Time: 0.34 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.js
@@ -1,12 +1,14 @@
 function solution(s, skip, index) {
     const filteredAlphabet = Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 97)).filter(elem => !skip.includes(elem));
+    const alphabetMap = new Map();
+    filteredAlphabet.forEach((char, i) => alphabetMap.set(char, i));
     
     s = s.split("");
     let answer = '';
     
     // 한 글자씩 변환
     s.forEach(str => {
-        const strIdx = filteredAlphabet.indexOf(str);
+        const strIdx = alphabetMap.get(str);
         const newStr = filteredAlphabet[(strIdx + index) % filteredAlphabet.length];
         answer += newStr;
     })
