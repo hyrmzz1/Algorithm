b199b7eb248351dc931bc3b7c7e137858f92335c
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-10-12 20:29:49 +0900

    [level 2] Title: 할인 행사, Time: 28.48 ms, Memory: 41 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,16 +1,37 @@
 function solution(want, number, discount) {
     let answer = 0;
+    const discountMap = new Map();
+    const wantMap = new Map();
+    want.forEach((elem, idx) => {
+        wantMap.set(elem, number[idx]);
+    })
     
-    for (let i = 0; i < discount.length; i++) {
-        const list = discount.slice(i, i + 10);
-        let flag = true;
+    // 초기 슬라이딩 윈도우
+    for (let i = 0; i < 10; i++) {
+        // discountMap.has(discount[i]) ? discountMap.set(discount[i], discountMap.get(discount[i]) + 1) : discountMap.set(discount[i], 1);
+        discountMap.set(discount[i], (discountMap.get(discount[i]) || 0) + 1);
+    }
+    
+    // discountMap이 wantMap에 있는 요소를 가지고 있는지 판별
+    const isMatching = (map1, map2) => {
+        for (let [key, value] of map2) {
+            // 원하는 개수보다 할인 중인 개수가 적을 때 (또는 해당 요소가 없을 때)
+            if ((map1.get(key) || 0) < value) return false;
+        }
+        return true;
+    }
+    
+    if (isMatching(discountMap, wantMap)) answer++;
+    
+    // 슬라이딩 윈도우
+    for (let i = 10; i < discount.length; i++) {
+        // 맨 앞 요소 빼기
+        discountMap.set(discount[i - 10], discountMap.get(discount[i - 10]) - 1);
+        // i번째 요소 더하기
+        discountMap.set(discount[i], (discountMap.get(discount[i]) || 0) + 1);
         
-        want.forEach((w, idx) => { 
-            // 원하는 만큼 할인구매 X
-            if (list.filter(elem => elem === w).length < number[idx]) flag = false;
-        })
-        
-        if (flag) answer++; // 원하는 만큼 할인구매 가능한 날
+        // isMatching 함수 돌리기
+        if (isMatching(discountMap, wantMap)) answer++;
     }
     
     return answer;
