803e6e5a6f386284405150746374d34a178cc349
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-10-24 20:15:11 +0900

    [level 3] Title: 여행경로, Time: 5.15 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.js
+++ 프로그래머스/3/43164. 여행경로/여행경로.js
@@ -1,53 +1,32 @@
 function solution(tickets) {
     const answer = [];
+    tickets.sort(); // 가능한 경로 중 알파벳 순으로 빠른 것 반환하기 위함
+    console.log(tickets)
+    const visited = new Array(tickets.length).fill(0); // ticket 사용 여부
     
-    tickets = tickets.sort();
-    const visited = new Array(tickets.length).fill(0);    // 티켓 사용 여부
-
-    const dfs = (from, path) => {
-        if (path.length === tickets.length) {
-            answer.push(...path, from);
-            return true;
+    const DFS = (departure, arr) => {
+        arr.push(departure);
+        
+        if (arr.length === tickets.length + 1) {
+            answer.push(...arr);
+            return true;    // 경로 찾음
         }
         
         for (let i = 0; i < tickets.length; i++) {
-            if (visited[i] === 0 && tickets[i][0] === from) {
-                visited[i] = 1; // 사용 처리
-                if (dfs(tickets[i][1], [...path, from])) {
-                    return true;
-                }
-                visited[i] = 0; // 경로 실패
+            const [from, to] = tickets[i];
+            
+            if (from === departure && !visited[i]) {
+                visited[i] = 1; // 방문처리
+                if (DFS(to, arr)) return true;   // 경로 완성
+                visited[i] = 0; // 잘못된 길 -> 방문처리 취소
             }
         }
         
-        return false;   // 경로 없을 때만 여기 도달
+        arr.pop();  // 잘못된 경로
+        return false;   // 경로 없음
     }
     
-    dfs('ICN', []);
+    DFS('ICN', []);
     
     return answer;
 }
-
-// 백트래킹 적용 X - tickets = [["ICN", "BBB"], ["BBB", "ICN"], ["ICN", "AAA"]]; 일 때 오류 발생
-/*
-function solution(tickets) {
-    const answer = [];
-    const visited = [];
-    
-    tickets = tickets.sort();
-    
-    const dfs = (from, tickets) => {
-        answer.push(from);
-        
-        if (tickets.length > 0) {
-            let toIdx = tickets.findIndex(elem => elem[0] === from);
-            let to = tickets[toIdx][1];
-            tickets.splice(toIdx, 1);
-            dfs(to, tickets);
-        }
-    }
-    
-    dfs('ICN', tickets);
-    return answer;
-}
-*/
