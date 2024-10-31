6361c0ad1d447552df4c240c7456f6c2e11b57f1
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-10-31 21:26:06 +0900

    [level 2] Title: 디펜스 게임, Time: 7050.24 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.js
@@ -5,30 +5,34 @@ function solution(n, k, enemy) {
     
     while (left <= rigth) {
         let mid = Math.floor((left + rigth) / 2);
-        let soldier = n;    // 병사 수
-        let skill = k;  // 사용할 수 있는 무족권 개수
         
         // 0 ~ mid까지의 라운드 막을 수 있는지 확인
         const round = enemy.slice(0, mid).sort((a, b) => b - a);
-        let finRound = mid; // 
         
-        round.forEach((roundEnemy, idx) => {
-            if (skill > 0) {
-                skill--;    // 무족권 사용
-            } else {
-                // 무족권 다 썼다면 직접 적 막아야 함
-                if (soldier >= roundEnemy) {
-                    soldier -= roundEnemy;
-                } else {
-                    finRound = idx; // 최종 도달 라운드
-                }
+        // 0 ~ mid까지의 라운드에 모두 무족권 사용할 수 있는 경우 -> 통과
+        if (mid <= k) {
+            left = mid + 1;
+            continue;
+        }
+        
+        let bool = true;
+        let sum = 0;
+        // 무족권 스킬 없이 구간 통과 가능 여부 확인
+        for (let i = k; i < round.length; i++) {
+            sum += round[i];
+            
+            // 이 라운드 통과 못함
+            if (sum > n) {
+                bool = false;
+                break;
             }
-        })
+        }
     
-        if (finRound >= mid) {
-            // 이 구간 모두 통과 가능
+        if (bool) {
+            // 라운드 통과 가능
             left = mid + 1;
         } else {
+            // 라운드 통과 불가
             rigth = mid -1;
         }
     }
