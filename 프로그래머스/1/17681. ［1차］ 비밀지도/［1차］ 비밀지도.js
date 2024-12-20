edf4b49d3c5bc76131063a15ef19aed904fa4474
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-12-20 23:40:56 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.19 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,11 +1,23 @@
 function solution(n, arr1, arr2) {
-    const answer = new Array(n);
+    const answer = [];
     
-    for (let i = 0; i < n; i++) {
-        answer[i] = (arr1[i] | arr2[i])
-            .toString(2)
-            .padStart(n, 0)
-            .replace(/1|0/g, (j) => j === '1' ? j = '#' : j = ' ');
+    // 1. n x n 배열로 변환
+    for (let i = 0; i < arr1.length; i++) {
+        arr1[i] = arr1[i].toString(2).padStart(n, "0");
+        arr2[i] = arr2[i].toString(2).padStart(n, "0");
+    }
+    
+    // 2. 두 배열 겹치는 부분 계산
+    for (let i = 0; i < arr1.length; i++) {
+        let r = "";
+        for (let j = 0; j < arr1[0].length; j++) {
+            if (arr1[i][j] === "0" && arr2[i][j] === "0") {
+                r += " ";
+            } else {
+                r += "#";
+            }
+        }
+        answer.push(r);
     }
     
     return answer;
