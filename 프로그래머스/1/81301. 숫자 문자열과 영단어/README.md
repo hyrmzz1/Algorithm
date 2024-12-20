2c19882b976e05702c103c61152169f44dff4c09
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-12-20 23:24:19 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,10 +1,10 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
-메모리: 33.4 MB, 시간: 0.05 ms
+메모리: 33.5 MB, 시간: 0.08 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 25일 23:21:52
+2024년 12월 20일 23:24:16
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.js
@@ -1,9 +1,20 @@
 function solution(s) {
-    let words = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];    // index: 0~9. 총 10개
+    const strToNum = new Map([
+        [0, "zero"],
+        [1, "one"],
+        [2, "two"],
+        [3, "three"],
+        [4, "four"],
+        [5, "five"],
+        [6, "six"],
+        [7, "seven"],
+        [8, "eight"],
+        [9, "nine"],
+    ]);
     
-    for(let i = 0; i < words.length; i++){
-        s = s.split(words[i]);
-        s = s.join(i)
+    for (const [key, value] of strToNum.entries()) {
+        s = s.replaceAll(value, key)
+        if(!isNaN(Number(s))) break;
     }
     
     return Number(s);
