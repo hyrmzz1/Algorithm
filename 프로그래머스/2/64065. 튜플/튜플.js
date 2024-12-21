d348c09587238fd0252b4be11169d76ec5a3bb36
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-12-21 19:35:03 +0900

    [level 2] Title: 튜플, Time: 44.61 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.js
+++ 프로그래머스/2/64065. 튜플/튜플.js
@@ -1,19 +1,14 @@
 function solution(s) {
-    let arr = s.replaceAll('{', '')
-                .replace('}}', '')
-                .split('},');
-    
-    let tmpArr = [];
-    for (let i = 0; i < arr.length; i++) {
-        tmpArr.push(arr[i].split(','))
-    }
-    tmpArr.sort((a, b) => a.length - b.length);
-    
     const answer = [];
-    tmpArr.forEach(innerArr => {
-        innerArr.forEach(elem => {
-            if (!answer.includes(Number(elem))) answer.push(Number(elem));
-        })
+    // 1. s에서 각 집합({})을 숫자로 이루어진 배열로 만들기
+    const array = s.slice(2, -2).split("},{").map(group => group.split(',').map(Number));
+    
+    // 2. 집합 길이별로 나열
+    array.sort((a, b) => a.length - b.length)
+    
+    // 3. answer에 없는 요소 넣기
+    array.forEach(group => {
+        answer.push(group.filter(elem => !answer.includes(elem))[0])
     })
     
     return answer;
