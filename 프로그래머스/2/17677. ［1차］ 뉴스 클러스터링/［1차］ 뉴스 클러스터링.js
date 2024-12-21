219ec76ba3e9e47d2fbb8fa39aaeb04c650c6516
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-12-21 20:31:56 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 0.96 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.js
@@ -1,32 +1,37 @@
 function solution(str1, str2) {
-    let arr1 = [], arr2 = [];
+    let arr1 = [];    // str1 다중집합
+    let arr2 = [];    // str2 다중집합
     
+    // 1. 문자열 두 글자씩 끊기, 모두 소문자로 처리
     for (let i = 0; i < str1.length - 1; i++) {
-        arr1.push(str1[i]+str1[i+1])
+        arr1.push(str1[i] + str1[i + 1])
     }
     
     for (let i = 0; i < str2.length - 1; i++) {
-        arr2.push(str2[i]+str2[i+1])
+        arr2.push(str2[i] + str2[i + 1])
     }
     
-    // arr 원소 다 소문자로 변환, 영문으로만 이루어진 원소로만 구성
-    arr1 = arr1.map(elem => elem.toLowerCase()).filter(elem => /^[a-zA-Z]+$/.test(elem));
-    arr2 = arr2.map(elem => elem.toLowerCase()).filter(elem => /^[a-zA-Z]+$/.test(elem));
+    // 2. 해당 원소에 영문자가 아닌 게 있을 경우 원소 제거
+    arr1 = arr1.filter(elem => /^[a-zA-Z]*$/.test(elem)).map(elem => elem.toLowerCase());
+    arr2 = arr2.filter(elem => /^[a-zA-Z]*$/.test(elem)).map(elem => elem.toLowerCase());
     
-    // 두 집합 모두 공집합일 때
-    if (arr1.length === 0 && arr2.length === 0) return 65536;
+    // 3. 공집합 처리
+    if (arr1.length === 0 && arr2.length === 0) return 65536;   // 유사도는 1
     
-    let union = arr1.length + arr2.length;    // 아직 교집합 안뺀 상태
+    // 4. 자카드 유사도 계산
+    let answer = 65536;
+    let union = arr1.length + arr2.length;  // 여기에 교집합 개수 빼야 함
+    let inter = arr2.length;    // 여기에 차집합 개수 빼야 함
     
-    let inter = 0;  // 교집합 요소 개수
-    arr1.forEach(elem => {
-        if (arr2.includes(elem)) {
-            arr2.splice(arr2.indexOf(elem), 1);
-            inter++;
-        }
-    })
+    // 4-1. 교집합 개수 구하기
+    for (const elem of arr1) {
+        let idx = arr2.indexOf(elem);
+        if (idx === -1) continue;
+        arr2.splice(idx, 1);
+    }
     
-    union -= inter;  // 합집합 요소 개수
+    inter -= arr2.length;
+    union -= inter;
     
-    return Math.floor((inter / union) * 65536);
+    return Math.floor(answer * (inter / union));
 }
