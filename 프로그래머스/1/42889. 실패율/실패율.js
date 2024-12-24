07c49f75c8085439a5a9359fb72a8d80f91ca2c4
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-12-24 17:08:50 +0900

    [level 1] Title: 실패율, Time: 6823.88 ms, Memory: 67.2 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.js
+++ 프로그래머스/1/42889. 실패율/실패율.js
@@ -1,15 +1,10 @@
 function solution(N, stages) {
     const answer = []  // [스테이지 번호, 실패율]
     
-    let user = stages.length;   // 사용자
-    let stage = 1;  // 스테이지 번호
-    stages.sort((a, b) => a - b);
-    
-    while (stage < N + 1) {
-        let failUser = stages.filter((elem) => elem === stage).length;    // 해당 스테이지 클리어하지 못한 사용자 수
-        answer.push([stage, failUser / user]);
-        stage++;
-        user -= failUser;
+    for (let i = 1; i <= N; i++) {
+        let notClear = stages.filter(elem => elem === i).length;    // 도달, 클리어 X
+        let reach = stages.filter(elem => elem >= i).length;    // 도달
+        answer.push([i, notClear / reach]);
     }
     
     answer.sort((a, b) => b[1] - a[1]);
