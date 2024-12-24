a4f9a8b6961d3f5b2adb4a4cf442b88b59d2e421
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-12-24 17:54:10 +0900

    [level 2] Title: k진수에서 소수 개수 구하기, Time: 10.04 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
+++ 프로그래머스/2/92335. k진수에서 소수 개수 구하기/k진수에서 소수 개수 구하기.js
@@ -1,13 +1,15 @@
 const isPrime = (num) => {
+    if (num < 2 || !num) return false;
+    
     for (let i = 2; i <= Math.sqrt(num); i++) {
         if (num % i === 0) return false;
     }
+    
     return true;
 }
 
 function solution(n, k) {
-    n = n.toString(k);
-    const numbers = n.split("0").map(Number).filter(elem => Boolean && elem > 1);
+    const numbers = n.toString(k).split("0").map(Number);
     
     let cnt = 0;    // 소수 개수
     numbers.forEach(number => {
