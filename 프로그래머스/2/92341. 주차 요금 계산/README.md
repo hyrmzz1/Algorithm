ebdb54047d6f6edd3c61e97640e4d231e8430f5c
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2024-12-27 14:52:19 +0900

    [level 2] Title: 주차 요금 계산, Time: 10.09 ms, Memory: 34.7 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341#qna) 
 
 ### 성능 요약
 
-메모리: 33.6 MB, 시간: 0.17 ms
+메모리: 34.7 MB, 시간: 10.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 10일 18:15:27
+2024년 12월 27일 14:52:16
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.js
@@ -1,38 +1,47 @@
+// 차량 별 누적 시간 계산
+const accMinutes = (timeArr) => {
+    let minutes = 0;    // 누적 출자 시간
+
+    for (let i = 0; i < timeArr.length; i += 2) {
+        let [inHours, inMinutes] = timeArr[i].split(":").map(Number);
+        let [outHours, outMinutes] = timeArr[i + 1].split(":").map(Number);
+
+        minutes += (outHours * 60 + outMinutes) - (inHours * 60 + inMinutes)
+    }
+
+    return minutes;
+}
+
+// 주차 요금 계산
+const getFees = (accMin, fees) => {
+    if (accMin <= fees[0]) {
+        return fees[1];
+    } else {
+        return fees[1] + Math.ceil((accMin - fees[0]) / fees[2]) * fees[3];
+    }
+};
+
 function solution(fees, records) {
-    const answer = {};  // {차량번호: [in, out]}
-    const carId = [];
+    const answer = [];
     
+    // 차량 번호
+    const cars = new Map([...new Set(records.map(record => (record.split(" ")[1])).sort((a, b) => Number(a) - Number(b)))].map(car => [car, []]));
+    
+    // 차량별 입/출차 시간 기록
     records.forEach(record => {
-        const [time, id, inout] = record.split(' ');
-        carId.push(id);
-        
-        if (!answer[id]) answer[id] = [];
-
-        answer[id].push(time);    // in, out, in, out
-    });
-    
-    // OUT 없다면 23:59 추가하고 주차 요금 계산
-    const carNum = [...new Set(carId)].sort((a, b) => a - b);
-    const fee = []; // carNum과 같은 인덱스 -> 같은 차에 대한 값
-    
-    carNum.forEach(num => {
-        if (answer[num].length % 2 === 1) answer[num].push("23:59");
-
-        // 누적시간 계산
-        let accTime = 0;
-        for (let i = 0; i < answer[num].length; i += 2) {
-            let timeIn = answer[num][i];
-            let timeOut = answer[num][i + 1];  
-
-            let [hoursIn, minutesIn] = timeIn.split(':').map(Number);
-            let [hoursOut, minutesOut] = timeOut.split(':').map(Number);
-            let minutesDiff = hoursOut * 60 + minutesOut - (hoursIn * 60 + minutesIn);
-            accTime += minutesDiff;
-        }
-        
-        // 주차 요금
-        accTime <= fees[0] ? fee.push(fees[1]) : fee.push(fees[1] + Math.ceil((accTime - fees[0]) / fees[2]) * fees[3]);
+        const [time, carNum, inOut] = record.split(" ");
+        cars.get(carNum).push(time);
     })
     
-    return fee;
+    for (const [key, value] of cars) {
+        // 출차 내역 없는 경우
+        if (value.length % 2 !== 0) {
+            value.push("23:59");
+        }
+        
+        const accMin = accMinutes(value); // 누적 시간 계산
+        answer.push(getFees(accMin, fees)); // 요금 계산
+    }
+    
+    return answer;
 }
