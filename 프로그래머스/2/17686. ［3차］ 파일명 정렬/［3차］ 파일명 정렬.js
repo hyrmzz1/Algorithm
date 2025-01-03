74229f9a78594a959669dbce14992859e2605a31
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-01-03 22:58:10 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 19.94 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,32 +1,14 @@
 function solution(files) {
-    const answer = files.map((file) => {
-        const numIdx = file.search(/\d/gi);
-        
-        // HEAD, NUMBER, TAIL 파싱
-        const head = file.slice(0, numIdx);
-        const body = file.slice(numIdx);    // NUMBER + TAIL
-        const num = body.match(/^\d{1,5}/)[0];
-        const tail = body.slice(num.length);
-        
-        return [head, num, tail];
+    const regex = /^([^\d]+)(\d{0,5})(.*)$/
+    
+    const answer = files.map((file, idx) => {
+        let [, head, number, tail] = file.match(regex)
+        return { original: file, head: head.toLowerCase(), number: parseInt(number, 10) };
     })
     
-    // 대소문자 구분 없이 head 사전순 정렬
     answer.sort((a, b) => {
-        const upperA = a[0].toUpperCase();
-        const upperB = b[0].toUpperCase();
-        
-        if (upperA < upperB) return -1;
-        else if (upperA > upperB) return 1;
-        else {  // number 오름차순 정렬
-            const intA = Number(a[1]);
-            const intB = Number(b[1]);
-            
-            if (intA < intB) return -1;
-            else if (intA > intB) return 1;
-            else return 0;
-        }
+        return a.head.localeCompare(b.head) || a.number - b.number;
     })
     
-    return answer.map(ans => ans.join(''));
+    return answer.map(file => file.original);
 }
