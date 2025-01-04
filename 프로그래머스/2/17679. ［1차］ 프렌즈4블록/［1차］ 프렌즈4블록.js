86d4dae35b6fc4b468f65a24732c1f926c1ea8b5
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-01-04 20:09:15 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 24.17 ms, Memory: 38.6 MB -BaekjoonHub

--- 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
+++ 프로그래머스/2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.js
@@ -1,45 +1,50 @@
 function solution(m, n, board) {
+    let answer = 0; // 지워지는 블록 개수
     board = board.map(col => col.split(''));
     
     while (true) {
-        // 각 라운드
-        // const newBoard = [...board];    // 얕은 복사
-        const newBoard = board.map(row => [...row]);    // 2차원 배열 깊은 복사
-        let removeBool = false;    // 사라질 블록 유무
+        // 1. 2×2 모양 찾기
+        let is2x2 = false;
+        const deepCopyBoard = board.map(row => [...row]);   // 겹치는 부분 처리 위해 복사
         
         for (let i = 0; i < m - 1; i++) {
             for (let j = 0; j < n - 1; j++) {
                 if (
                     board[i][j] !== '' &&
-                    board[i][j] === board[i][j+1] &&
-                    board[i][j] === board[i+1][j] &&
-                    board[i][j] === board[i+1][j+1]
-                ){
-                    newBoard[i][j] = '';
-                    newBoard[i][j+1] = '';
-                    newBoard[i+1][j] = '';
-                    newBoard[i+1][j+1] = '';
-                    removeBool = true;
+                    board[i][j] === board[i][j + 1] &&
+                    board[i][j] === board[i + 1][j] &&
+                    board[i][j] === board[i + 1][j + 1]
+                ) {
+                    is2x2 = true;
+                    deepCopyBoard[i][j] = '';
+                    deepCopyBoard[i][j+1] = '';
+                    deepCopyBoard[i+1][j] = '';
+                    deepCopyBoard[i+1][j+1] = '';
                 }
             }
         }
         
-        if (!removeBool) break;  // 사라질 블록 없을 경우 게임 끝
+        // 1-1. 2×2 모양 없다면 반복문 종료
+        if (!is2x2) break;
         
-        // 재정렬
-        const reorderBoard = Array.from(Array(m), () => Array(n).fill(''));
-        for (let col = 0; col < n; col++) {
-            let newRow = m - 1;
+        // 1-2. board에 지워진 부분 모두 적용
+        board = deepCopyBoard;
+        
+        // 2. 2×2 모양 블록 지우고 위에 있는 블록 떨어뜨리기
+        for (let col = 0; col < n; col++) {   // 각 열 순회
             for (let row = m - 1; row >= 0; row--) {
-                if (newBoard[row][col] !== '') {
-                    reorderBoard[newRow][col] = newBoard[row][col];
-                    newRow--;
+                if (board[row][col] === '') {
+                    for (let k = row - 1; k >= 0; k--) {    // 위로 올라가며 알파벳 찾기
+                        if (board[k][col] !== '') {
+                            board[row][col] = board[k][col];
+                            board[k][col] = '';
+                            break;
+                        }
+                    }
                 }
             }
         }
-        
-        board = reorderBoard.map(v => [...v]);
     }
     
-    return board.map(row => row.filter(elem => elem === '').length).reduce((a, b) => a + b);
+    return board.flat().filter(elem => elem === '').length;
 }
