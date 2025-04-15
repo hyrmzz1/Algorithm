b21b8b8a06909b8ce470dc9bb5bd1093015fe43f
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-04-15 22:20:59 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 35.90 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,23 +1,26 @@
 function solution(gems) {
     let answer = [1, gems.length];
     const type = new Set(gems).size;
-    const gemsMap = new Map();
     
-    let left = 0, right = 0;    // 투포인터 (인덱스)
+    let left = 0;
+    let right = 0;
+    const gemsMap = new Map();
     gemsMap.set(gems[0], 1);
     
     while (right < gems.length) {
-        if (gemsMap.size === type) {    // map에 모든 gem 종류 있다면
-            if (answer[1] - answer[0] > right - left) answer = [left + 1, right + 1];   // 가장 짧은 구간 갱신
+        if (gemsMap.size === type) {
+            // 최단 구간 갱신
+            if (answer[1] - answer[0] > right - left) {
+                answer = [left + 1, right + 1];
+            }
             
-            // 더 짧은 구간 찾기
             gemsMap.set(gems[left], gemsMap.get(gems[left]) - 1);
             if (gemsMap.get(gems[left]) === 0) gemsMap.delete(gems[left]);
+            
             left++;
-        } 
-        else {    // map에 gem 종류 부족하다면
+        } else {
             right++;
-            gemsMap.set(gems[right], gemsMap.get(gems[right]) ? gemsMap.get(gems[right]) + 1 : 1);
+            gemsMap.set(gems[right] , gemsMap.has(gems[right]) ? gemsMap.get(gems[right]) + 1 : 1);
         }
     }
     
