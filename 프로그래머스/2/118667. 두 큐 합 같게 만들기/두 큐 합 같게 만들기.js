7f34e1be991ca9bdfc366fd26eeae613d14f43f7
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-04-15 19:27:36 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 26.85 ms, Memory: 56.7 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,29 +1,34 @@
 function solution(queue1, queue2) {
+    let cnt = 0;
+    
+    const len = queue1.length;
     let sum1 = queue1.reduce((acc, cur) => acc + cur);
     let sum2 = queue2.reduce((acc, cur) => acc + cur);
     
-    if ((sum1 + sum2) % 2 !== 0) return -1;
+    if ((sum1 + sum2) % 2) return -1;
     
-    let cnt = 0;    // 작업 횟수
-    const maxCnt = queue1.length * 3;
-    let firstIdx1 = 0;
-    let firstIdx2 = 0;
-    const initLength = queue1.length;
+    let pointer1 = 0;
+    let pointer2 = 0;
     
-    // 어떤 방법으로도 각 큐의 원소 합 같게 만들 수 없는 경우
-    // 서로의 원소를 맞바꾼 상태가 됐을 때 (swap)
     while (sum1 !== sum2) {
-        if (cnt > maxCnt) return -1;
+        if (pointer1 >= len && pointer2 >= len) return -1;  // 두 큐가 스왑상태일 때
         
         if (sum1 > sum2) {
-            sum1 -= queue1[firstIdx1];  // shift
-            queue2.push(queue1[firstIdx1]);  // push
-            sum2 += queue1[firstIdx1++];  // shift 대신 인덱스 증가
+            queue2.push(queue1[pointer1]);
+            
+            sum1 -= queue1[pointer1];
+            sum2 += queue1[pointer1];
+            
+            pointer1++;
         } else {
-            sum2 -= queue2[firstIdx2];
-            queue1.push(queue2[firstIdx2]);
-            sum1 += queue2[firstIdx2++];
+            queue1.push(queue2[pointer2]);
+            
+            sum1 += queue2[pointer2];
+            sum2 -= queue2[pointer2];
+            
+            pointer2++;
         }
+        
         cnt++;
     }
     
