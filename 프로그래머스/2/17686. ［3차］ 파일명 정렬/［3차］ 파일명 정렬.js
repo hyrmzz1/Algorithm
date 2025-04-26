f9fcc4f76547610f8389eea3badbbdf94120c1f1
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-04-26 15:37:48 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 606.83 ms, Memory: 39.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.js
@@ -1,14 +1,24 @@
+// 1. 파일명을 HEAD, NUMBER, TAIL로 나누기
+// 2. 정렬
 function solution(files) {
-    const regex = /^([^\d]+)(\d{0,5})(.*)$/
+    const answer = [];
+
+    // NUMBER -> 한 글자에서 최대 다섯 글자 사이의 연속된 숫자
+    // HEAD -> NUMBER 전, TAIL -> NUMBER 후
+    const numberRegex = /\d{1,5}/i;
     
-    const answer = files.map((file, idx) => {
-        let [, head, number, tail] = file.match(regex)
-        return { original: file, head: head.toLowerCase(), number: parseInt(number, 10) };
+    files.forEach(file => {
+        const number = file.match(numberRegex)[0];
+        const numberIdx = file.search(numberRegex);
+        
+        const head = file.slice(0, numberIdx);
+        const tail = file.slice(numberIdx + number.length);
+        
+        answer.push([head, number, tail]);
     })
     
-    answer.sort((a, b) => {
-        return a.head.localeCompare(b.head) || a.number - b.number;
-    })
+    answer.sort((a, b) => a[0].toLowerCase().localeCompare(b[0].toLowerCase())
+                || Number(a[1]) - Number(b[1]))
     
-    return answer.map(file => file.original);
+    return answer.map(arr => arr.join(''));
 }
