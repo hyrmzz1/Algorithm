f2c33f4859489c39cc6d6bb796b80c194524cb64
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-04-26 16:54:36 +0900

    [level 3] Title: 불량 사용자, Time: 65.07 ms, Memory: 37.6 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.js
@@ -1,37 +1,36 @@
 function solution(userId, bannedId) {
-    const bannedArr = [];
-    const visited = new Array(userId).fill(false);
+    // 1. 각 bannedId에 해당하는 아이디 배열 구하기
+    const bannedUserGroup = [];
     
-    // 1. 제재 아이디
-    bannedId.map(id => {
-        const regex = new RegExp('^' + id.replaceAll('*', '.') + '$');
-        bannedArr.push(userId.filter(uid => regex.test(uid)));
-    })
+    for (const bId of bannedId) {
+        const regex = new RegExp('^' + bId.replaceAll('*', '.') + '$');
+        bannedUserGroup.push(userId.filter(uId => regex.test(uId)));
+    }
     
-    // 2. 경우의 수 - dfs 돌며 방문하지 않은 아이디에 대한 경우의 수만 카운트
-    const result = new Set();
-    const temp = [];
+    // 2. 1에서 구한 배열들간의 조합 개수 구하기 - 백트래킹
+    const answer = new Set();
+    const usedId = new Set();
     
-    const dfs = (depth) => {
-        if (depth === bannedArr.length) {
-            result.add(temp.slice().sort().join(','));  // 중복 없도록 하기 위함
+    const backtrack = (path, idx) => {
+        if (idx === bannedId.length) {
+            answer.add([...path].sort().toString());
             return;
         }
         
-        bannedArr[depth].forEach(bannedUser => {
-            const idx = userId.indexOf(bannedUser);
-
-            if (!visited[idx]) {    // 방문 X
-                visited[idx] = true;    // 방문처리
-                temp.push(bannedUser);
-                dfs(depth + 1);
-                temp.pop(bannedUser);   // 백트래킹
-                visited[idx] = false;
-            }
-        })
+        for (const uId of bannedUserGroup[idx]) {
+            if (usedId.has(uId)) continue;
+            
+            path.push(uId);
+            usedId.add(uId);
+            
+            backtrack(path, idx + 1);
+            
+            path.pop();
+            usedId.delete(uId);
+        }
     }
     
-    dfs(0);
+    backtrack([], 0);
     
-    return result.size;
+    return answer.size;
 }
