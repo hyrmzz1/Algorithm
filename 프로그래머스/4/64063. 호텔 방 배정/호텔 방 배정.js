c24a4d891f92c42bb4b38bdf9119054a40a9c01e
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-05-13 21:49:24 +0900

    [level 4] Title: 호텔 방 배정, Time: 255.66 ms, Memory: 87.9 MB -BaekjoonHub

--- 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.js
+++ 프로그래머스/4/64063. 호텔 방 배정/호텔 방 배정.js
@@ -1,24 +1,24 @@
-const findRoom = (reqRoom, rooms) => {
-    // 원하는 방이 비어 있을 경우
-    if (!rooms.has(reqRoom)) {
-        rooms.set(reqRoom, reqRoom + 1);
-        return reqRoom;
-    }
-    
-    // 이미 배정되어 있을 경우
-    const nextRoom = rooms.get(reqRoom);
-    const emptyRoom = findRoom(nextRoom, rooms);
-    rooms.set(reqRoom, emptyRoom + 1);
-    
-    return emptyRoom;
-}
-
 function solution(k, roomNumber) {
     const answer = [];
     const rooms = new Map();
     
+    const findRoom = (reqRoom) => {
+        // 원하는 방이 비어 있을 경우
+        if (!rooms.has(reqRoom)) {
+            rooms.set(reqRoom, reqRoom + 1);
+            return reqRoom;
+        }
+
+        // 이미 배정되어 있을 경우
+        const nextRoom = rooms.get(reqRoom);
+        const emptyRoom = findRoom(nextRoom);
+        rooms.set(reqRoom, emptyRoom + 1);
+
+        return emptyRoom;
+    }
+    
     roomNumber.forEach(num => {
-        const room = findRoom(num, rooms);
+        const room = findRoom(num);
         answer.push(room);
     })
     
