cc622ebb36a837f677b10ef601ca516a48342ece
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-07-01 22:19:56 +0900

    [level 3] Title: 정수 삼각형, Time: 4.83 ms, Memory: 42 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -1,18 +1,12 @@
-// top-down
+// bottom-up
 function solution(triangle) {
-    const dp = triangle.map(row => [...row]);
+    const dp = triangle.map(row => [...row])
     
-    for (let i = 1; i < triangle.length; i++) {
-        for (let j = 0; j < triangle[i].length; j++) {
-            if (j === 0) {
-                dp[i][j] += dp[i - 1][j];
-            } else if (j === triangle[i].length - 1) {
-                dp[i][j] += dp[i - 1][j - 1];
-            } else {
-                dp[i][j] += Math.max(dp[i - 1][j], dp[i - 1][j - 1]);
-            }
+    for (let i = dp.length - 2; i >= 0; i--) {
+        for (let j = 0; j < dp[i].length; j++) {
+            dp[i][j] += Math.max(dp[i + 1][j], dp[i + 1][j + 1]);
         }
     }
     
-    return Math.max(...dp[triangle.length - 1]);
+    return dp[0][0];
 }
