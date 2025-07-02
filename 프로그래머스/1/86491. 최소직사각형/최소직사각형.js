010c6da4e28b6b0ea3de746c096099599dec91b6
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-07-02 16:40:43 +0900

    [level 1] Title: 최소직사각형, Time: 8.68 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -1,8 +1,9 @@
 function solution(sizes) {
+    let answer = 1;
     const sortedSizes = sizes.map(row => row.sort((a, b) => b - a));
     
-    const width = sortedSizes.reduce((acc, cur) => cur[0] > acc ? cur[0] : acc , 1);
-    const height = sortedSizes.reduce((acc, cur) => cur[1] > acc ? cur[1] : acc , 1);
+    answer *= sortedSizes.reduce((acc, cur) => cur[0] > acc ? cur[0] : acc, 1);
+    answer *= sortedSizes.reduce((acc, cur) => cur[1] > acc ? cur[1] : acc, 1);
     
-    return width * height;
+    return answer;
 }
