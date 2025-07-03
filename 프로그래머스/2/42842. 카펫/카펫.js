6c277bc38709b5ccc8340b1d3eb7c5cd07e1483d
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-07-03 22:39:43 +0900

    [level 2] Title: 카펫, Time: 0.23 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.js
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -1,9 +1,19 @@
+// 1. yellow 배치 조합 구하기 (r * c = yellow)
+    // 1-1. yellow가 24일 때 배치 조합은 [[24, 1], [12, 2], [8, 3], [6, 4]]
+// 2. r * 2 + c * 2 + 4 === brown 을 만족하는 r, c 구하기
 function solution(brown, yellow) {
-    for (let height = 3; height <= (brown + yellow); height++) {
-        let width = (brown + yellow) / height;
+    const pairs = [];
+    
+    // 1. yellow 배치 조합 구하기
+    for (let c = 1; c * c <= yellow; c++) {
+        if (yellow % c !== 0) continue;   // 약수 아님
         
-        if ((height - 2) * (width - 2) === yellow) {
-            return [width, height];
-        }
+        const r = yellow / c;
+        pairs.push([r, c]);
+    }
+    
+    // 2. r * 2 + c * 2 + 4 === brown을 만족하는 격자 크기 구하기
+    for (const [r, c] of pairs) {
+        if (brown === r * 2 + c * 2 + 4) return [r + 2, c + 2];
     }
 }
