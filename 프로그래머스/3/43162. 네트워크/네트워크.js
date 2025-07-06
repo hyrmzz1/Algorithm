40b72ec44caa66bfed44a79b0e4e4fe6cab46e11
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-07-06 20:57:17 +0900

    [level 3] Title: 네트워크, Time: 0.62 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.js
+++ 프로그래머스/3/43162. 네트워크/네트워크.js
@@ -1,24 +1,40 @@
+// Union-find
 function solution(n, computers) {
-    let cnt = 0;
-    const visited = new Array(n).fill(0);
+    const parent = Array.from({ length: n }, (_, idx) => idx);
     
-    const dfs = (computer) => {
-        visited[computer] = 1; // 방문 처리
+    // 부모 노드를 찾는 함수
+    const find = (x) => {
+        if (parent[x] === x) return x;
+        
+        return parent[x] = find(parent[x])
+    }
 
-        // 인접 + 방문하지 않은 노드 방문
-        for (let i = 0; i < n; i++) {
-            if (i !== computer && computers[i][computer] === 1 && visited[i] === 0) {
-                dfs(i);
+    // 두 노드를 합치는 함수
+    const union = (x, y) => {
+        const xRoot = find(x);
+        const yRoot = find(y);
+
+        if (xRoot === yRoot) return;    // 이미 합쳐져 있는 경우
+        return parent[yRoot] = xRoot;
+    }
+
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            if (i === j) continue;  // 자기 자신
+            
+            // 두 컴퓨터가 연결된 경우
+            if (computers[i][j]) {
+                union(i, j);
             }
         }
     }
     
-    for (let i = 0; i < n; i++) {   // 컴퓨터 전체 돌면서
-        if (visited[i] === 0) { // 아직 방문하지 않았다면
-            dfs(i); // 방문 + 인접 컴퓨터 탐색
-            cnt++;  // 한 뭉텅이 -> 네트워크 개수 +1
-        }
+    // parents = [0, 0, 0, 0, 2] 임에도 하나의 네트워크로 연결될 수 있다.
+    const roots = new Set();
+    
+    for (let node of parent) {
+        roots.add(find(node));
     }
     
-    return cnt;
+    return roots.size;
 }
