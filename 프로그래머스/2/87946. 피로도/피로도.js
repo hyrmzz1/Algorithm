87bf63f24ec4e23202bb07003f3ac56be6273b37
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-07-06 18:49:22 +0900

    [level 2] Title: 피로도, Time: 23.86 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,48 +1,25 @@
-// 완전 탐색 (모든 경우의 수를 생성하고 각각을 확인)
-
-// arr에 대한 순열 구하는 함수
-const getPermutations = (arr) => {
-    if (arr.length === 1) return arr;
+function solution(k, dungeons) {
+    let maxCount = 0;
+    const visited = new Array(dungeons.length).fill(false);
     
-    const result = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        const current = arr[i];
-        const remaining = arr.slice(0, i).concat(arr.slice(i + 1));
-
-        const permutations = getPermutations(remaining);
-
-        for (let perm of permutations) {
-            result.push([current].concat(perm));
+    const dfs = (nowPiro, count) => {
+        maxCount = Math.max(count, maxCount);
+        
+        for (let i = 0; i < dungeons.length; i++) {
+            const [need, somo] = dungeons[i];
+            
+            // 방문 가능 여부 확인
+            if (nowPiro < need) continue;
+            if (visited[i]) continue;
+            
+            visited[i] = true;
+            dfs(nowPiro - somo, count + 1);
+            
+            visited[i] = false;
         }
     }
-
-    return result;
-}
-
-function solution(k, dungeons) {
-    let answer = 0; // 탐색 가능한 최대 던전 수
-    const dungeonIdx = Array.from({ length: dungeons.length }, (_, idx) => idx);
     
-    // 1. 던전 탐색 순서 순열 구하기
-    const permutations = getPermutations(dungeonIdx);
+    dfs(k, 0);
     
-    // 2. 각각의 순열에서 최대 던전 수 구하기
-    permutations.forEach(perm => {
-        let piro = k;
-        let count = 0;  // 해당 순열에서 탐색 가능한 던전 수
-        
-        perm.forEach(dungeonIdx => {
-            const [need, somo] = dungeons[dungeonIdx];
-            
-            if (piro >= need) {
-                piro -= somo;
-                count++;
-            }
-        })
-        
-        answer = Math.max(answer, count);
-    })
-    
-    return answer;
+    return maxCount;
 }
