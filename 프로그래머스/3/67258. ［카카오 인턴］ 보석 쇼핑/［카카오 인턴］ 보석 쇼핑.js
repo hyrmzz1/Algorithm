0c1e250cbc78efcfe8503419173cdfac5cb11696
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-07-06 15:51:36 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 40.06 ms, Memory: 39.3 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.js
@@ -1,29 +1,41 @@
+// 슬라이딩 윈도우 - 투 포인터 ("배열에서 연속된 부분구간", "최소 구간")
 function solution(gems) {
-    let answer = [0, gems.length - 1];
-    const type = new Set(gems).size;
+    const gemsType = new Set(gems);
     
-    let left = 0;
-    let right = 0;
-    const gemsMap = new Map();
-    gemsMap.set(gems[0], 1);
+    let left = 0, right = 0;
+    let minLength = gems.length;    // 구간 길이 ([0, 0]일 때는 1)
+    let answer = [1, gems.length];
+    const gemsMap = new Map();  // key: 보석 종류, value: 개수
     
+    // gemsType을 모두 포함하는 가장 짧은 구간 구하기
     while (right < gems.length) {
-        if (gemsMap.size === type) {
-            // 최단 구간 갱신
-            if (right - left < answer[1] - answer[0] || 
-               (right - left === answer[1] - answer[0]) && left < answer[0]) {
-                answer = [left, right];
+        // 오른쪽 포인터 Map에 반영
+        const rigthGem = gems[right];
+        gemsMap.set(rigthGem, (gemsMap.get(rigthGem) || 0) + 1);
+        
+        // 모든 보석 포함된 경우
+        while (gemsMap.size === gemsType.size) {
+            // minLength와 현재 구간 길이 비교
+            if (right - left + 1 < minLength) {
+                // 더 짧은 구간이면 minLength, answer 업데이트
+                minLength = right - left + 1;
+                answer = [left + 1, right + 1];
             }
             
-            gemsMap.set(gems[left], gemsMap.get(gems[left]) - 1);
-            if (gemsMap.get(gems[left]) === 0) gemsMap.delete(gems[left]);
+            // 왼쪽 포인터 확장 및 보석 제거
+            const leftGem = gems[left];
+            gemsMap.set(leftGem, (gemsMap.get(leftGem) || 0) - 1);
+            
+            if (gemsMap.get(leftGem) === 0) {
+                gemsMap.delete(leftGem);
+            }
             
             left++;
-        } else {
-            right++;
-            gemsMap.set(gems[right] , gemsMap.has(gems[right]) ? gemsMap.get(gems[right]) + 1 : 1);
         }
+        
+        // 오른쪽 포인터 확장
+        right++;
     }
     
-    return [++answer[0], ++answer[1]];
+    return answer;
 }
