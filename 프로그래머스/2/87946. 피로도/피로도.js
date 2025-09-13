c5f1355334861289c5ad4a4af1812116422c3cc2
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-09-13 17:47:03 +0900

    [level 2] Title: 피로도, Time: 5.73 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,23 +1,26 @@
+// DFS+백트래킹
+// DFS로 경로 탐색 (DFS 내에서 매번 for문으로 경로를 생성함.)
+// 해당 경로 방문 가능 여부 확인 후 불가능하면 백트래킹, 가능하면 DFS 마저 진행
 function solution(k, dungeons) {
-    let maxCount = 0;
+    let maxCount = 0; // 탐험한 최대 던전 수
     const visited = new Array(dungeons.length).fill(false);
     
-    const dfs = (nowPiro, count) => {
-        maxCount = Math.max(count, maxCount);
+    const dfs = (currPiro, count) => {
+        maxCount = Math.max(maxCount, count);
         
         for (let i = 0; i < dungeons.length; i++) {
-            const [need, somo] = dungeons[i];
-            
-            // 방문 가능할 경우
-            if (nowPiro >= need && !visited[i]) {            
-                visited[i] = true;
-                dfs(nowPiro - somo, count + 1);
-                visited[i] = false;
+            // 탐험 불가
+            if (visited[i] || currPiro < dungeons[i][0]) {
+                continue;
             }
+            
+            visited[i] = true;
+            dfs(currPiro -  dungeons[i][1], count + 1);
+            visited[i] = false;
         }
     }
     
-    dfs(k, 0);
+    dfs(k, maxCount);
     
     return maxCount;
 }
