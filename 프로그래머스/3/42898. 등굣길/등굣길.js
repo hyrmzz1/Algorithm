93f8a80b67d549709f85ab06909245efa1790b69
Hyerim Yang <80061287+hyrmzz1@users.noreply.github.com>
2025-09-14 21:46:36 +0900

    [level 3] Title: 등굣길, Time: 1.35 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.js
+++ 프로그래머스/3/42898. 등굣길/등굣길.js
@@ -1,19 +1,29 @@
 function solution(m, n, puddles) {
+    const MOD = 1000000007;
     const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
-    dp[1][1] = 1;   // 초기값 세팅 (집)
     
-    const puddleSet = new Set();
-    for (const [c, r] of puddles) {
-        puddleSet.add(`${r}-${c}`);
+    // 웅덩이 표시
+    for (const [i, j] of puddles) {
+        dp[j][i] = -1;
     }
+    
+    dp[1][1] = 1;   // 시작점 표시
     
     for (let i = 1; i <= n; i++) {
         for (let j = 1; j <= m; j++) {
-            // 집, 웅덩이는 패스
-            if (i === 1 && j === 1) continue;
-            if (puddleSet.has(`${i}-${j}`)) continue;
+            // 웅덩이 -> 스킵
+            if (dp[i][j] === -1) {
+                continue;
+            }
             
-            dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % 1000000007;
+            if (i === 1 && j === 1) {
+                continue;
+            }
+            
+            const top = dp[i - 1][j] === -1 ? 0 : dp[i - 1][j];
+            const left = dp[i][j - 1] === -1 ? 0 : dp[i][j - 1];
+            
+            dp[i][j] = (top + left) % MOD;
         }
     }
     
